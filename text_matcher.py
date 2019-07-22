7cf17e8c63d3edaa7bab355d6d338e5323c40b29
anna-croley <v-ancrol@microsoft.com>
2019-07-22 18:27:28 -0400

    Update text_matcher.py

--- text_matcher.py
+++ text_matcher.py
@@ -2,7 +2,7 @@ import documents
 
 class SentenceMatcher:
     ''' The SentenceMatcher takes in:
-        sentence1: an instance of the Sentence class
+         sentence1: an instance of the Sentence class
          sentence2: an instance of the Sentence class
          jaccard_threshold (type: float): a cut-off value for the jaccard index, below which the two sentences will be
                             considered definitely not a match and assigned a match value of "No"
@@ -65,7 +65,7 @@ class History:
     def __init__(self, match_candidate):
         self.candidates = [match_candidate]
 
-    def get_length(self):
+    def length(self):
         ''' Output: An int, the number of match candidates in the list self.candidates '''
         return len(self.candidates)
 
@@ -79,23 +79,40 @@ class History:
         new_j = candidate.sent2_id
         return (i + 1 == new_i and j + 1 == new_j)
 
-
     def flush_history(self, length):
         '''  '''
+        if self.length() >= length:
+            if self.length() >= min_consec_maybe:
+                copied_sentences = history.candidates
+            else:
+                for candidate in self.candidates:
+                    if candidate.match == "Yes":
+                        copied_sentences = history_matches
+                        break
+
 
     def add_candidate(self, candidate):
         ''' Add candidate '''
         self.candidates += [candidate]
 
-    def rule_out_history(self, ):
-        if  :
+    def rule_out_history(self, length):
+        if self.length() < length:
             del self
 
 
 class DocumentMatcher:
-    def __init__(self, document1, document2, min_consec_candidates):
-        self.document1 = document1.id_wordbags()
-        self.document2 = document2.id_wordbags()
-        self.min_consec_candidates = min_consec_candidates
+    def __init__(self, target_document, source_document, min_consec_maybe):
+        self.target = document1.id_wordbags()
+        self.source = document2.id_wordbags()
+        self.min_consec_candidates = min_consec_maybe
+
+    def get_match_candidates(self, jaccard_threshold, min_sentence_length):
+        for sentence in self.target:
+            for sentence in self.source:
+                SentenceMatcher()
+
+
+
+
 
 
