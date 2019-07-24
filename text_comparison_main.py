39b6dd67d37ff050a0ca01d7fd60c9b21e76a0f6
asprout <asprout@github.com>
2019-07-24 09:50:20 -0400

    deleted unused files

--- text_comparison_main.py
+++
@@ -1,9 +0,0 @@
-''' '''
-class Main:
-
-
-target_doc = Document(config.target_file_name)
-source_doc = Document(config.source_file_name)
-text_matcher = DocumentMatcher(target_doc, source_doc)
-text_matcher.set_candidates()
-self.candidates
