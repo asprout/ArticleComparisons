03cc09059d6032fb4ec3c552022f68fb7f38c306
asprout <asprout@github.com>
2019-07-29 17:11:08 -0400

    Reorganized code and added additional methods to improve usability; added comments

--- documents.py
+++ documents.py
@@ -1,83 +1,82 @@
-import string # remove punctuation
-import numpy as np 
 from collections import Counter # contains word counter function
 import nltk # tokenize words and sentences based on language
 # Following line may be necessary to run nltk calls:
 # nltk.download('punkt')
 
 class Document:
-    def __init__(self, text, text_encoding = "utf8", text_language = "english"):
+    def __init__(self, text, clean = False, stem = True,
+                text_encoding = "utf8", text_language = "english"):
         self.text = text 
+        self.clean = clean # Used to determine if text is already preprocessed
+        self.stem = stem # Should words be stemmed?
         self.text_encoding = text_encoding
         self.text_language = text_language
-        self.paragraphs = self.parse_paragraphs()
-        self.sentences = self.parse_sentences()
-        self.bow_sentences = self.parse_bow_sentences()
+        self.paragraphs = None
+        self.sent_para_map = None # Stores the paragraphs of each sentence 
+        self.sentences = None
+        self.bow_sent_map = None # Stores the sentence of each bow
+        self.bow_sentences = None
+        self.parse_text()
 
     def __str__(self):
-        return self.text
+        if self.clean:
+            return self.text
+        return ("\n").join(self.paragraphs)
 
-    def parse_paragraphs(self, para_sep = "\n"):
-        return self.text.split(para_sep)
+    def parse_text(self, para_sep = "\n", min_para = 4, remove_dups = True):
+        self.parse_paragraphs(para_sep, min_para)
+        self.parse_sentences(remove_dups)
+        self.parse_bow_sentences(remove_dups)
 
-    def parse_sentences(self):
+    def parse_paragraphs(self, para_sep = "\n", min_para = 4):
+        if self.clean:
+            self.paragraphs = self.text.split(para_sep)
+        else: # Clean up bad text from scraped article
+            paras = self.text.split("###")
+            paras = [p for p in paras if len(p.split()) >= min_para and not p.isupper()]
+            self.paragraphs = paras 
+        return self.paragraphs
+
+    def parse_sentences(self, remove_dups = True):
+        """ From self.paragraphs, processes and stores a list of sentences and 
+        their paragraph mappings, ignoring duplicates
+        """
         sentences = []
-        for para in self.paragraphs:
-            sentences.extend(nltk.sent_tokenize(para, self.text_language))
-        return sentences
+        sent_para_map = []
+        for i, para in enumerate(self.paragraphs):
+            append_sents = nltk.sent_tokenize(para, self.text_language)
+            if remove_dups:
+                append_sents = [sent for sent in append_sents if sent not in sentences]
+            for sent in append_sents:
+                sentences.append(sent)
+                sent_para_map.append(i)
+        self.sentences = sentences 
+        self.sent_para_map = sent_para_map
+        return self.sentences
 
-    def parse_bow_sentences(self):
-        ''' Returns a list of dictionaries for each sentence, 
-        where the keys are words and values are counts of the words in 
-        the sentence 
+    def parse_bow_sentences(self, remove_dups = True):
+        ''' Returns a list of dictionaries for each sentence in self.sentences,
+        where the keys are words and values are counts within the sentences
         '''
         bow_sentences = []
-        for sent in self.sentences:
-            tokens = [token for token in nltk.word_tokenize(sent.lower()) 
-                                                        if token.isalnum()]
-            bow_sentences.append(tokens)
-        return [dict(Counter(tokens)) for tokens in bow_sentences]
+        bow_sent_map = []
+        tokenizer = nltk.RegexpTokenizer(r'\w+')
+        ps = nltk.PorterStemmer()
+        for i, sent in enumerate(self.sentences):
+            tokens = tokenizer.tokenize(sent)
+            if self.stem:
+                tokens = [ps.stem(token) for token in tokens]
+            if len(tokens) > 0 and (tokens not in bow_sentences or not remove_dups):
+                bow_sentences.append(dict(Counter(tokens)))
+                bow_sent_map.append(i)
+        self.bow_sentences = bow_sentences
+        self.bow_sent_map = bow_sent_map
+        return self.bow_sentences
+
+    def get_sentences(self):
+        if len(self.bow_sent_map) > 0:
+            return [self.sentences[i] for i in self.bow_sent_map]
+        return self.sentences
 
     def get_bow_sentences(self):
         return self.bow_sentences
-
-
-class DocumentComparisons:
-    def __init__(self, jaccard_threshold = .9, min_sentence = 4):
-        self.jaccard_threshold = .9
-        self.min_sentence = 4
-
-    def jaccard_index(self, bow_a, bow_b):
-        ''' Takes two BOW dictionaries and returns the jaccard index, defined as
-            Jaccard(A, B) = |A and B|/|A or B|
-        '''
-        intsec_words = set(bow_a) & set(bow_b)
-        intsec_vals = [min(bow_a.get(word), bow_b.get(word)) for 
-                                                        word in intsec_words]
-        intsec = sum(intsec_vals)
-        union = sum(bow_a.values()) + sum(bow_b.values()) - intsec
-        return float(intsec / union)
-
-
-class ArticleComparisons:
-    def __init__(self, source, target):
-        self.source = source
-        self.target = target
-        self.comparer = DocumentComparisons()
-
-    def jaccard_matrix(self):
-        ''' With an m-sentence source document and n-sentence target, 
-        returns a m x n symmetric matrix with jaccard indices between 
-        the sentences 
-        '''
-        source_bow = self.source.get_bow_sentences()
-        target_bow = self.target.get_bow_sentences()
-        jac_mat = np.zeros((len(source_bow), len(target_bow)))
-        for i in range(len(source_bow)):
-            for j in range(len(target_bow)):
-                jac_mat[i, j] = self.comparer.jaccard_index(source_bow[i], 
-                                                            target_bow[j])
-        return jac_mat
-
-
-
--- textcomparisons.py
+++ textcomparisons.py
@@ -1,98 +1,37 @@
-import string # remove punctuation
 import numpy as np 
-from collections import Counter # contains word counter function
-import nltk # tokenize words and sentences based on language
-# Following line may be necessary to run nltk calls:
-# nltk.download('punkt')
-
-class Document:
-    def __init__(self, text, clean = False, stem = True,
-                text_encoding = "utf8", text_language = "english"):
-        self.text = text 
-        # Boolean representing if the article is 'clean'
-        # To change how parse_paragraphs is called
-        self.clean = clean 
-        self.stem = stem
-        self.text_encoding = text_encoding
-        self.text_language = text_language
-        self.paragraphs = self.parse_paragraphs()
-        self.sentences = self.parse_sentences()
-        self.bow_indices = []
-        self.bow_sentences = self.parse_bow_sentences()
-
-    def __str__(self):
-        if self.clean:
-            return self.text
-        return ("\n").join(self.paragraphs)
-
-    def parse_paragraphs(self, para_sep = "\n"):
-        if self.clean:
-            self.paragraphs = self.text.split(para_sep)
-        else: # Clean up bad text from scraped article
-            paras = self.text.split("###")
-            paras = [p for p in paras if len(p.split()) > 3 and not p.isupper()]
-            self.paragraphs = paras 
-        return self.paragraphs
-
-    def parse_sentences(self):
-        sentences = []
-        for para in self.paragraphs:
-            sentences.extend(nltk.sent_tokenize(para, self.text_language))
-        self.sentences = sentences if self.clean else list(Counter(sentences))
-        return self.sentences
-
-    def parse_bow_sentences(self):
-        ''' Returns a list of dictionaries for each sentence, 
-        where the keys are words and values are counts within the sentences
-        '''
-        bow_sentences = []
-        bow_indices = []
-        tokenizer = nltk.RegexpTokenizer(r'\w+')
-        ps = nltk.PorterStemmer()
-        for i, sent in enumerate(self.sentences):
-            tokens = [token for token in tokenizer.tokenize(sent)]
-            if self.stem:
-                tokens = [ps.stem(token) for token in tokens]
-            if len(tokens) > 0 and tokens not in bow_sentences:
-                bow_sentences.append(tokens)
-                bow_indices.append(i)
-        self.bow_sentences = [dict(Counter(tokens)) for tokens in bow_sentences]
-        self.bow_indices = bow_indices 
-        return self.bow_sentences
-
-    def get_sentences(self):
-        ''' Returns the sentences from the article that correspond to actual
-        text (i.e. len(tokens) > 0)
-        '''
-        if len(self.bow_indices) > 0:
-            return [self.sentences[i] for i in self.bow_indices]
-        return None
-
-    def get_bow_sentences(self):
-        return self.bow_sentences
-
+import matplotlib.pyplot as plt
+import documents
 
 class DocumentComparisons:
-    def __init__(self, jaccard_threshold = .5, same_threshold = .9):
+    def __init__(self, thresh_jaccard = .25, thresh_same_sent = .9):
+        self.thresh_jaccard = thresh_jaccard
+        self.thresh_same_sent = thresh_same_sent 
+        # Store information about the last comparison done
         self.last_source = None
         self.last_target = None
         self.last_jaccard_matrix = None
         self.last_jaccard_weights = None 
         self.last_jaccard_matches = None
-        self.jaccard_threshold = jaccard_threshold
-        self.same_threshold = same_threshold # When two sentences are identical
 
-    def jaccard_matrix(self, source = None, target = None, weighted = False):
-        ''' With an m-sentence source document and n-sentence target, 
-        returns a m x n symmetric matrix with jaccard indices between 
-        the sentences. If weighted, then the indices within the 
-        matrix will be weighted by average pairwise sentence length. 
+    def jaccard_index(self, bow_a, bow_b, visualize = False):
+        ''' Takes two BOW dictionaries and returns the jaccard index, defined as
+            Jaccard(A, B) = |A and B|/|A or B|
         '''
-        if source is None or target is None: # Just return stored matrices
-            if weighted:
-                return self.last_jaccard_weights * self.last_jaccard_matrix
-            return self.last_jaccard_matrix
-        # Otherwise compute matrices given the source and target 
+        intsec_words = set(bow_a) & set(bow_b)
+        intsec_vals = [min(bow_a[word], bow_b[word]) for word in intsec_words]
+        intsec = sum(intsec_vals)
+        union = sum(bow_a.values()) + sum(bow_b.values()) - intsec
+        index = float(intsec / max(1.0, union))
+        if visualize:
+            print("Jaccard Index:", index)
+            print("U:", intsec_words)
+            print("I:", set(bow_a) - set(bow_b), "\n", set(bow_b) - set(bow_a))
+        return index
+
+    def jaccard_matrix(self, source, target):
+        ''' With an m-sentence source document and n-sentence target, 
+        returns a m x n symmetric matrix with pairwise jaccard indices
+        '''
         source_bow = source.get_bow_sentences()
         target_bow = target.get_bow_sentences()
         jac_mat = np.zeros((len(source_bow), len(target_bow)))
@@ -107,52 +46,49 @@ class DocumentComparisons:
         self.last_target = target
         self.last_jaccard_matrix = jac_mat
         self.last_jaccard_weights = weight_mat
-        if weighted:
-            return self.last_jaccard_weights * self.last_jaccard_matrix
         return self.last_jaccard_matrix
 
-    def jaccard_index(self, bow_a, bow_b):
-        ''' Takes two BOW dictionaries and returns the jaccard index, defined as
-            Jaccard(A, B) = |A and B|/|A or B|
-        '''
-        intsec_words = set(bow_a) & set(bow_b)
-        intsec_vals = [min(bow_a.get(word), bow_b.get(word)) for 
-                                            word in intsec_words]
-        intsec = sum(intsec_vals)
-        union = sum(bow_a.values()) + sum(bow_b.values()) - intsec
-        return float(intsec / union)
+    def get_jaccard_matrix(self, source = None, target = None, weighted = False):
+        """ Returns the last Jaccard matrix or computes a new one if source
+        or target is provided. If weighted, then the indices within the 
+        matrix will be weighted by average pairwise sentence length. 
+        """
+        if source is not None or target is not None:
+            self.jaccard_matrix(source, target)
+        if weighted: 
+            return self.last_jaccard_matrix * self.last_jaccard_weights
+        return self.last_jaccard_matrix
 
-    def match_matrix(self, source = None, target = None, threshold = None):
+    def match_matrix(self, source = None, target = None, thresh_jaccard = None):
         ''' Match the sentences between source and target, 
         where multiple sentences can map onto one
         Note: uses jaccard_matrix if available
         '''
-        if threshold is None:
-            threshold = self.jaccard_threshold
-        jac_mat = self.jaccard_matrix(source, target)
+        if thresh_jaccard is not None:
+            self.thresh_jaccard = thresh_jaccard
+        jac_mat = self.get_jaccard_matrix(source, target)
         matches = np.zeros(jac_mat.shape)
         # assign sentences to match maximum jaccard indices
         for i in range(matches.shape[0]):
             maxmatch = np.argmax(jac_mat[i, :])
-            if jac_mat[i, maxmatch] > threshold:
+            if jac_mat[i, maxmatch] > self.thresh_jaccard:
                 matches[i, maxmatch] = 1
         for j in range(matches.shape[1]):
             maxmatch = np.argmax(jac_mat[:, j])
-            if jac_mat[maxmatch, j] > threshold:
+            if jac_mat[maxmatch, j] > self.thresh_jaccard:
                 matches[maxmatch, j] = 1
 
         for i in range(matches.shape[0]):
             matches[i, :] = self.weigh_matches(matches[i, :], jac_mat[i, :])
-            #matches[i, :] = self.del_nonconsecutives(matches[i, :])
         for j in range(matches.shape[1]):
             matches[:, j] = self.weigh_matches(matches[:, j], jac_mat[:, j])
-            #matches[:, j] = self.del_nonconsecutives(matches[:, j])
 
         self.last_jaccard_matches = matches
-        self.jaccard_threshold = threshold
         return matches
 
-    def get_match_matrix(self):
+    def get_match_matrix(self, source = None, target = None, thresh_jaccard = None):
+        if any([source is not None, target is not None, thresh_jaccard is not None]):
+            self.match_matrix(source, target, thresh_jaccard)
         return self.last_jaccard_matches
 
     def weigh_matches(self, matches, jaccards):
@@ -164,9 +100,17 @@ class DocumentComparisons:
         if total <= 1:
             return matches
         maxmatch = np.argmax(jaccards)
-        if jaccards[maxmatch] > self.same_threshold:
+        if (self.thresh_same_sent is not None and 
+                                    jaccards[maxmatch] > self.thresh_same_sent):
             return [0] * maxmatch + [1] + [0] * (len(jaccards) - maxmatch - 1)
         return matches/total
+
+    def jaccard_score(self, source = None, target = None, weighted = False):
+        match_mat = self.get_match_matrix(source, target)
+        jac_mat = self.get_jaccard_matrix(weighted = weighted)
+        jac_score = np.sum(jac_mat * match_mat)
+        #return jac_score / np.mean(jac_mat.shape)
+        return jac_score / np.min(jac_mat.shape)
 
     def del_nonconsecutives(self, arr):
         ''' Takes an array and leaves only the maximum consecutive sequence
@@ -188,15 +132,12 @@ class DocumentComparisons:
                 maxcount = curcount 
         return [0] * maxind + [1] * maxcount + [0] * (len(arr) - (maxind + maxcount))
 
-    def print_sentence_matches(self, threshold = None):
-        if self.last_jaccard_matches is None:
-            return
-        if threshold is None:
-            threshold = self.jaccard_threshold
-        matches = self.match_matrix(threshold = threshold)
-        jac_mat = self.last_jaccard_matrix
-        source_sents = self.last_source.get_sentences()
-        target_sents = self.last_target.get_sentences()
+    def print_sentence_matches(self, thresh_jaccard = None):
+        jac_mat = self.get_jaccard_matrix()
+        matches = self.get_match_matrix(thresh_jaccard = thresh_jaccard)
+        if matches is not None:
+            source_sents = self.last_source.get_sentences()
+            target_sents = self.last_target.get_sentences()
         for i in range(matches.shape[0]):
             if np.sum(matches[i, :] > 0):
                 print("S", i, ":", source_sents[i], "\n")
@@ -204,41 +145,72 @@ class DocumentComparisons:
                     print("\tT", j, np.round(jac_mat[i, j], 2), ":", target_sents[j], "\n")
 
 
-class ArticleComparisons:
-    def __init__(self, docs = {}):
-        self.docs = docs # Holds documents in a dictionary by id
-        self.comparer = DocumentComparisons()
+class ArticleComparisons(DocumentComparisons):
+    def __init__(self, thresh_jaccard = .25, thresh_same_sent = .9, thresh_same_doc = .25):
+        DocumentComparisons.__init__(self, thresh_jaccard, thresh_same_sent)
+        self.thresh_same_doc = thresh_same_doc
+        self.weighted = False
+        self.docs = {}
         self.score_mat = None 
+        self.clusters = [] # List of cluster sets (by id)
 
-    def add_doc(self, name, doc):
-        self.docs["name"] = doc 
+    def update_docs(self, docs):
+        """ If docs is not None, updates the documents stored in class instance, 
+        and returns whether or not the class instance was updated
+        """
+        if docs is not None and docs is not self.docs:
+            self.docs = docs 
+            return True 
+        return False
 
-    def add_docs(self, docs):
-        self.docs.update(docs)
+    def article_clusters(self, docs = None):
+        score_mat = self.jac_score_mat(docs)
+        thresh = self.thresh_same_doc 
+        unique = set()
 
     def jac_score_mat(self, docs = None, weighted = False):
-        ''' Returns the matrix of match scores based on pairwise
-        document Jaccard matrices in docs
+        ''' Returns stored matrix of pairwise document match stores, 
+        or computes and returns new matrix if docs is not None, or weighted is
+        different from self.weighted 
         '''
-        if docs is None: # Return stored information 
-            if self.score_mat is None and len(self.docs) > 0:
-                return jac_score_mat(self.docs, weighted)
-            return self.score_mat 
+        if all([self.score_mat is not None, 
+               not self.update_docs(docs), 
+               weighted == self.weighted]):
+            return self.score_mat
 
         score_mat = np.zeros((len(docs), len(docs)))
+
         for i, doc1id in enumerate(docs):
             for j, doc2id in enumerate(docs):
-                doc1 = docs[doc1id]
-                doc2 = docs[doc2id]
                 if (i > j):
                     score_mat[i, j] = score_mat[j, i]
                 else:
-                    jac_mat = self.comparer.jaccard_matrix(doc1, doc2, weighted)
-                    match_mat = self.comparer.match_matrix()
-                    jac_score = np.sum(jac_mat * match_mat)
-                    score_mat[i, j] = jac_score / np.mean(jac_mat.shape)
+                    score_mat[i, j] = self.jaccard_score(docs[doc1id], docs[doc2id])
         self.score_mat = score_mat 
         return score_mat
+
+    def display_mat(self, mat, normalize = False, xlabs = None, ylabs = None):
+        """ Uses matplotlib to display mat; 
+        if normalize is True, then coloring is normalized by value range
+        """
+        fig = plt.figure(figsize = (20, 20))
+        ax = fig.add_subplot(111)
+        if normalize:
+            ax.matshow(mat, cmap = plt.cm.Blues)
+        else:
+            ax.matshow(mat, cmap = plt.cm.Blues, vmin = 0, vmax = 1)
+        for i in range(mat.shape[0]):
+            for j in range(mat.shape[1]):
+                ax.text(j, i, round(mat[i, j], 2), va = "center", ha = "center")
+        ax.set_xticks([x for x in range(-1, mat.shape[1] + 1)])
+        ax.set_yticks([x for x in range(-1, mat.shape[0] + 1)])
+        if xlabs is not None: 
+            ax.set_xticklabels([''] + xlabs)
+            plt.xticks(rotation = 90)
+            if ylabs is None and mat.shape[0] == mat.shape[1]:
+                ylabs = xlabs
+        if ylabs is not None:
+            ax.set_yticklabels([''] + ylabs)
 
 
 
