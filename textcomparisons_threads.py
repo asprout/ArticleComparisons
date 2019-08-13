bd5b65026483b5d5239d610950e8d5c6cb06ec89
asprout <asprout@github.com>
2019-08-13 17:29:11 -0400

    threading for document parsing

--- textcomparisons_threads.py
+++ textcomparisons_threads.py
@@ -42,9 +42,41 @@ class ParallelComparisons():
 			if tc.cosinesim(doc1.vec, doc2.vec) >= 0.9:
 				self.score_mat[doc_indices[0], doc_indices[1]] = comparer.jaccard_score(doc1, doc2)
 
-	def run(self, docs):
-		self.docs = docs 
-		self.docids = [i for i in docs.keys()]
+	def reader(self, df, para_sep, parser):
+		while True:
+			with self.lock:
+				if self.task_counter >= len(self.tasks):
+					break
+				doc_id = self.tasks[self.task_counter]
+				self.task_counter += 1
+			row = df["id"] == doc_id 
+			if parser is not None or df.loc[row, "doc"].iloc[0] is None:
+				df.loc[row, "doc"] = documents.Document(df.loc[row, "text"].iloc[0], para_sep, parser)
+			self.docs[doc_id] = df.loc[row, "doc"].iloc[0]
+
+	def loadDocs(self, df, ids, para_sep = "###", parser = None):
+		self.docs = {}
+		self.tasks = ids 
+		self.task_counter = 0
+		print("Preparing to read documents")
+		self.running = True 
+		progress = threading.Thread(target = self.progress)
+		progress.start()
+		threads = []
+		for core in range(multiprocessing.cpu_count()):
+			thread = threading.Thread(target = self.reader, args = (df, para_sep, parser))
+			thread.start()
+			threads.append(thread)
+		for thread in threads:
+			thread.join()
+		self.running = False
+
+		return self.docs 
+
+	def run(self, docs = None):
+		if docs is not None:
+			self.docs = docs 
+		self.docids = [i for i in self.docs.keys()]
 		self.tasks = tc.flatten([[[i, j] for j in range(i + 1, len(docs))] for i in range(len(docs))])
 		self.task_counter = 0 
 		self.score_mat = np.zeros((len(docs), len(docs)))
